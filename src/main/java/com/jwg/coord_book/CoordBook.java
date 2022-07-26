34b26f5fb457483ecfc0d77a265797bd104e8e13
JWGardiner <jamesgardiner340@gmail.com>
2022-07-26 16:46:11 +0100

    Texture and cut out some useless code

--- src/main/java/com/jwg/coord_book/CoordBook.java
+++ src/main/java/com/jwg/coord_book/CoordBook.java
@@ -1,6 +1,7 @@
 package com.jwg.coord_book;
 
 import com.jwg.coord_book.util.ensureFileStructureExists;
+import net.minecraft.util.Identifier;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
@@ -8,7 +9,7 @@ import org.slf4j.LoggerFactory;
 
 public class CoordBook implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Coordinate Book");
-
+	public static final Identifier BOOK_ICON = new Identifier("coordbook:textures/gui/book.png");
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("{} has started initializing!", mod.metadata().name());
