d1fef05df4e691cca12e318a90c1ca23b4c6b0b7
JWGardiner <jamesgardiner340@gmail.com>
2022-07-26 17:39:04 +0100

    Created a page counter (Placement will change), and added textures for the arrows

--- src/main/java/com/jwg/coord_book/CoordBook.java
+++ src/main/java/com/jwg/coord_book/CoordBook.java
@@ -10,11 +10,12 @@ import org.slf4j.LoggerFactory;
 public class CoordBook implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Coordinate Book");
 	public static final Identifier BOOK_ICON = new Identifier("coordbook:textures/gui/book.png");
+	public static final Identifier BACK_ICON = new Identifier("coordbook:textures/gui/back.png");
+	public static final Identifier NEXT_ICON = new Identifier("coordbook:textures/gui/next.png");
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("{} has started initializing!", mod.metadata().name());
-		boolean importantFilesExist = ensureFileStructureExists.exists("CoordinateBook/", "coords.json");
-		ensureFileStructureExists.createFiles(importantFilesExist);
+		ensureFileStructureExists.createFiles(ensureFileStructureExists.exists("CoordinateBook/"));
 		LOGGER.info("{} has finished initializing!", mod.metadata().name());
 	}
 }
