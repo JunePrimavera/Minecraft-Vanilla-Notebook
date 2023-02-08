25cfd4909427e5ba041869689917cf4729fb15ce
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-02-08 13:05:58 +0000

    Rewrote init code, refactored config code and updated version to 2.1.1. Fixed crash when loading for the first time

--- src/main/java/com/june/notebook/Notebook.java
+++ src/main/java/com/june/notebook/Notebook.java
@@ -1,9 +1,5 @@
 package com.june.notebook;
 
-import com.june.notebook.Util.ensureFileStructureExists;
-import com.june.notebook.Util.generateConfig;
-import com.june.notebook.Util.readConfig;
-import com.june.notebook.Util.resetConfig;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
@@ -17,8 +13,8 @@ import static com.june.notebook.Keybind.openBookKeybindRegister;
 public class Notebook implements ModInitializer {
 	public static boolean presetsEnabled = true;
 	public static String verified_page_location = "Notebook/Default";
-	public static final boolean developerMode = false;
-	public static final String version = "2.1.0";
+	public static final boolean developerMode = true;
+	public static final String version = "2.1.1";
 	public static final String project = "Vanilla-Notebook";
 	public static String pageLocation = "Notebook/Default";
 	public static final Logger LOGGER = LoggerFactory.getLogger(project);
@@ -29,16 +25,17 @@ public class Notebook implements ModInitializer {
 	public static final Identifier DELETE_ICON = new Identifier("notebook:textures/gui/del-page.png");
 	public static final Identifier CONFIG_ICON = new Identifier("notebook:textures/gui/config.png");
 
-	public static boolean NEEDS_SETUP = false;
 
 	public static void re_init(boolean is_p_init) {
-
-		readConfig.read();
+		// This code is FAR better than before, from ~80 lines to ~40
+		// Make sure the config files exist
+		if (!new File("config/vanilla-notebook/config.cfg").exists()) {
+			Config.generate(0);
+			Config.reset();
+		}
+		Config.read();
 		verified_page_location = pageLocation;
-		// I should probably improve this code
-		// It's very cursed but in a weird way i love it
-		LOGGER.info("{} has started initializing!", project);
-		ensureFileStructureExists.createFiles(ensureFileStructureExists.exists(pageLocation+"1/"));
+		// Make first pages of book if they don't exist already
 		if (!new File(pageLocation+"1/0.jdat").exists()) {
 			try {
 				boolean b = new File(pageLocation+"1").mkdirs();
@@ -47,14 +44,6 @@ public class Notebook implements ModInitializer {
 				throw new RuntimeException(e);
 			}
 		}
-		File firstPage = new File(pageLocation+"1/0.jdat");
-		LOGGER.info("Page folder is \"{}\"", pageLocation);
-		boolean tmp = false;
-		if (NEEDS_SETUP) { generateConfig.generate(0); resetConfig.reset();  }
-		try {
-			if (firstPage.createNewFile()){ LOGGER.info("Created first page of the book.."); NEEDS_SETUP = true; }
-		} catch (IOException e) { System.out.println(e); }
-		ensureFileStructureExists.createFiles(ensureFileStructureExists.exists(pageLocation+"2/"));
 		if (!new File(pageLocation+"2/0.jdat").exists()) {
 			try {
 				boolean b = new File(pageLocation+"2").mkdirs();
@@ -63,14 +52,6 @@ public class Notebook implements ModInitializer {
 				throw new RuntimeException(e);
 			}
 		}
-		firstPage = new File(pageLocation+"2/0.jdat");
-		LOGGER.info("Page folder is \"{}\"", pageLocation);
-		tmp = false;
-		if (NEEDS_SETUP) { generateConfig.generate(0); resetConfig.reset();  }
-		try {
-			if (firstPage.createNewFile()){ LOGGER.info("Created first page of the book.."); NEEDS_SETUP = true; }
-		} catch (IOException e) { System.out.println(e); }
-		ensureFileStructureExists.createFiles(ensureFileStructureExists.exists(pageLocation+"3/"));
 		if (!new File(pageLocation+"3/0.jdat").exists()) {
 			try {
 				boolean b = new File(pageLocation+"3").mkdirs();
@@ -79,41 +60,13 @@ public class Notebook implements ModInitializer {
 				throw new RuntimeException(e);
 			}
 		}
-		firstPage = new File(pageLocation+"3/0.jdat");
-		LOGGER.info("Page folder is \"{}\"", pageLocation);
-		tmp = false;
-		if (NEEDS_SETUP) { generateConfig.generate(0); resetConfig.reset();  }
-		try {
-			if (firstPage.createNewFile()){ LOGGER.info("Created first page of the book.."); NEEDS_SETUP = true; }
-		} catch (IOException e) { System.out.println(e); }
-
-
-
-		if (!new File("config/vanilla-notebook/config.cfg").exists()) NEEDS_SETUP = true;
-		try {
-			tmp = new File("config/vanilla-notebook/").mkdirs();
-			tmp = new File("config/vanilla-notebook/config.cfg").createNewFile();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		if (NEEDS_SETUP) resetConfig.reset();
-
-
-		readConfig.read();
-
-		if (!pageLocation.equals("notebook") && !new File(pageLocation+"1").exists()) { tmp = new File(pageLocation+"1").mkdirs(); }
-		if (!pageLocation.equals("notebook") && !new File(pageLocation+"2").exists()) { tmp = new File(pageLocation+"2").mkdirs(); }
-		if (!pageLocation.equals("notebook") && !new File(pageLocation+"3").exists()) { tmp = new File(pageLocation+"3").mkdirs(); }
-		if (tmp) { LOGGER.info("Possible first time use! Thank you for using my mod!"); }
-
+		// Register keybind
 		if (is_p_init) {
 			LOGGER.info("Registering keybinds...");
 			openBookKeybindRegister();
 			LOGGER.info("{} has finished initializing!", project);
 		}
-
 	}
-
 	@Override
 	public void onInitialize() { re_init(true); }
 
