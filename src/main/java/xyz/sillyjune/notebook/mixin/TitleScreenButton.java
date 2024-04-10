708c2c18124aa4ee622938b0d91fc12dc2181e45
Juniper Primavera <juniper.gardiner@gmail.com>
2024-04-10 10:55:40 +0100

    I think 8 months is a new procrastination high score

--- src/main/java/xyz/sillyjune/notebook/mixin/TitleScreenButton.java
+++ src/main/java/xyz/sillyjune/notebook/mixin/TitleScreenButton.java
@@ -25,11 +25,11 @@ public abstract class TitleScreenButton extends Screen implements BookScreen.Con
     }
     @Inject(at = @At("RETURN"), method="initWidgetsNormal")
     private void addCustomButton(int y, int spacingY, CallbackInfo ci) {
-        this.addDrawableChild(new TexturedButtonWidget((this.width / 2 + 104), y + spacingY + BUTTON_OFFSET, 20, 20, 0, 0, 20, MAIN_BUTTON_ICON, 32, 64, (button) -> {
+        this.addDrawableChild(new TexturedButtonWidget((this.width / 2 + 104), y + spacingY + BUTTON_OFFSET, 20, 20, MAIN_BUTTON_ICON, (button) -> {
             assert this.client != null;
             NotebookScreen.BookName = "Default";
             this.client.setScreen(new NotebookScreen());
-        }, Text.translatable("jwg.button.bookmenu")));
+        }, Text.translatable("june.button.bookmenu")));
     }
 }
 
