8a3d928d9b8534fff74ce947f0fed8114921e471
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:22:14 +0100

    Disable developer mode
    Signed-off-by: James Gardiner <86971433+JWGardiner@users.noreply.github.com>

--- src/main/java/com/jwg/coord_book/CoordBook.java
+++ src/main/java/com/jwg/coord_book/CoordBook.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class CoordBook implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Coordinate Book");
 	public static final Identifier BOOK_ICON = new Identifier("coordbook:textures/gui/book.png");
-	public static final boolean developerMode = true;
+	public static final boolean developerMode = false;
 	public static final String version = "0.2.0";
 	public static final String project = "Coordinate-Book";
 
