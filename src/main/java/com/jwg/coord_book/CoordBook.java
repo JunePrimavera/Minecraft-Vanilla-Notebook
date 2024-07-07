649af4e50f32d224d9251789ad3c8686f17f255c
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:22:14 +0100

    Removed old back/next arrow texture code

--- src/main/java/com/jwg/coord_book/CoordBook.java
+++ src/main/java/com/jwg/coord_book/CoordBook.java
@@ -13,8 +13,7 @@ import java.io.IOException;
 public class CoordBook implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Coordinate Book");
 	public static final Identifier BOOK_ICON = new Identifier("coordbook:textures/gui/book.png");
-	public static final Identifier BACK_ICON = new Identifier("coordbook:textures/gui/back.png");
-	public static final Identifier NEXT_ICON = new Identifier("coordbook:textures/gui/next.png");
+	
 	@Override
 	public void onInitialize(ModContainer mod) {
 		LOGGER.info("{} has started initializing!", mod.metadata().name());
