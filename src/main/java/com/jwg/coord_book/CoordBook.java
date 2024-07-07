f6904be756ee8b9af7f32bf7983c7ef05d2a0d7b
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:42 +0100

    Update version

--- src/main/java/com/jwg/coord_book/CoordBook.java
+++ src/main/java/com/jwg/coord_book/CoordBook.java
@@ -14,7 +14,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class CoordBook implements ModInitializer {
-	public static final boolean developerMode = true;
+	public static final boolean developerMode = false;
 	public static final String version = "0.3.3";
 	public static final String project = "Coordinate-Book";
 	public static String pageLocation = "CoordinateBook";
