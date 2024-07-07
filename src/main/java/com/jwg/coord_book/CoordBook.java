55ae20a33adf606d36822c18d4c72fe31f8266a4
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:08 +0100

    Ported 1.19 -> 1.18.2

--- src/main/java/com/jwg/coord_book/CoordBook.java
+++ src/main/java/com/jwg/coord_book/CoordBook.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 public class CoordBook implements ModInitializer {
 	public static final boolean developerMode = false;
-	public static final String version = "1.1.2";
+	public static final String version = "1.2.0";
 	public static final String project = "Coordinate-Book";
 	public static String pageLocation = "CoordinateBook";
 	public static final Logger LOGGER = LoggerFactory.getLogger(project);
@@ -31,37 +31,19 @@ public class CoordBook implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		LOGGER.info("{} has started initializing!", project);
-
 		ensureFileStructureExists.createFiles(ensureFileStructureExists.exists(pageLocation+"/"));
 		File firstPage = new File(pageLocation+"/0.jdat");
 		LOGGER.info("Page folder is \"{}\"", pageLocation);
 		boolean tmp = false;
 		try {
-			if (firstPage.createNewFile()){
-				LOGGER.info("Created first page of the book..");
-				NEEDS_SETUP = true;
-			}
-			else{
-				if (!new File("config/coordinate-book/config.cfg").exists()) {
-					NEEDS_SETUP = true;
-				}
-				tmp = new File("config/coordinate-book/config.cfg").createNewFile();
-			}
-		} catch (IOException e) {
-			System.out.println(e);
-		}
-		if (NEEDS_SETUP) {
-			generateConfig.generate(0);
-			resetConfig.reset();
-		}
+			if (firstPage.createNewFile()){ LOGGER.info("Created first page of the book.."); NEEDS_SETUP = true; }
+			else { if (!new File("config/coordinate-book/config.cfg").exists()) { NEEDS_SETUP = true; } tmp = new File("config/coordinate-book/config.cfg").createNewFile(); }
+		} catch (IOException e) { System.out.println(e); }
+		if (NEEDS_SETUP) { generateConfig.generate(0); resetConfig.reset(); }
 		readConfig.read();
 
-		if (!pageLocation.equals("CoordinateBook") && !new File(pageLocation).exists()) {
-			tmp = new File(pageLocation).mkdirs();
-		}
-		if (tmp) {
-			LOGGER.info("Possible first time use! Thank you for using my mod!");
-		}
+		if (!pageLocation.equals("CoordinateBook") && !new File(pageLocation).exists()) { tmp = new File(pageLocation).mkdirs(); }
+		if (tmp) { LOGGER.info("Possible first time use! Thank you for using my mod!"); }
 		LOGGER.info("{} has finished initializing!", project);
 	}
 }
--- src/main/java/com/jwg/coord_book/screens/menuScreen.java
+++ src/main/java/com/jwg/coord_book/screens/menuScreen.java
@@ -7,33 +7,26 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ScreenTexts;
-import net.minecraft.client.gui.screen.ingame.BookScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.gui.widget.PageTurnWidget;
 import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.util.NarratorManager;
+import net.minecraft.client.util.SelectionManager;
 import net.minecraft.client.util.math.MatrixStack;
-
 import net.minecraft.text.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.Nullable;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.*;
 import java.util.*;
+import java.util.List;
 
 import static com.jwg.coord_book.CoordBook.*;
-import static java.awt.event.KeyEvent.getExtendedKeyCodeForChar;
 
 @Environment(EnvType.CLIENT)
 public class menuScreen extends Screen {
 
-    boolean nextCharacterSpecial = false;
     public static int page = 0;
     public static int bookmarkedpage = 0;
     public static int pageLimit = -1;
@@ -44,11 +37,12 @@ public class menuScreen extends Screen {
     public static boolean deletePageButtonShown = true;
     private String versionText;
     private String contents;
-
+    private SelectionManager selectionManager;
+    private int loc;
     public menuScreen() {
         this(true);
     }
-
+    char ltchr;
     private menuScreen(boolean bl) {
         super(NarratorManager.EMPTY);
         this.contents = "";
@@ -56,17 +50,16 @@ public class menuScreen extends Screen {
         this.cachedPage = Collections.emptyList();
         this.pageIndexText = ScreenTexts.DONE;
         this.pageTurnSound = bl;
+        this.loc = 0;
+        this.ltchr = 0;
         pageLimit = pageLimit -1;
-
     }
 
-    protected void init() {
-        this.addButtons();
-    }
+    protected void init() { assert this.client != null; this.client.keyboard.setRepeatEvents(true); this.addButtons(); selectionMgr(); }
     protected void removePage(int rmpage) {
         int files = Objects.requireNonNull(new File(pageLocation + "/").list()).length;
         int pagesToRename = files - rmpage;
-        boolean tmp;
+        boolean tmp = false;
         if (rmpage == 0) { LOGGER.warn("Can't delete first page"); }
         else if (rmpage == files-1) { goToPreviousPage(); tmp = new File(pageLocation +"/"+ rmpage + ".jdat").delete(); }
         else {
@@ -89,36 +82,25 @@ public class menuScreen extends Screen {
     }
     protected void addButtons() {
         //Done button
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, 196, 200, 20, ScreenTexts.DONE, (button) -> {
-            assert this.client != null;
-            this.client.setScreen(null);
-        }));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, 196, 200, 20, ScreenTexts.DONE, (button) -> { assert this.client != null; this.client.setScreen(null); }));
 
         //Delete page button
         if (deletePageButtonShown) {
-            this.addDrawableChild(new TexturedButtonWidget(this.width -21, this.height-21, 20, 20, 0, 0, 20, DELETE_ICON, 32, 64, (button) -> {
-                removePage(page);
-            }, new TranslatableText("jwg.button.close")));
+            this.addDrawableChild(new TexturedButtonWidget(this.width -21, this.height-21, 20, 20, 0, 0, 20, DELETE_ICON, 32, 64, (button) -> removePage(page), new TranslatableText("jwg.button.close")));
         }
 
         //Bookmark button
         //The placement is temporary
-        //In 1.2.0 I will redo all of the GUI bits, so it's a bit nicer
+        //In the future I will redo all of the GUI bits, so it's a bit nicer
         //I feel like the GUI currently is not great
         if (bookmarkedpage != page) {this.addDrawableChild(new TexturedButtonWidget(this.width/2-45, 12, 20, 20, 0, 0, 20, BOOKMARK_ICON, 32, 64, (button) -> {bookmarkedpage = page; this.writeBookmark(); assert this.client != null;this.client.setScreen(this); }, new TranslatableText("jwg.button.bookmark")));}
-        else {this.addDrawableChild(new TexturedButtonWidget(this.width/2-45, 12, 20, 20, 0, 0, 20, BOOKMARK_ENABLED_ICON, 32, 64, (button) -> {bookmarkedpage = -1; this.writeBookmark(); this.client.setScreen(this); }, new TranslatableText("jwg.button.bookmark")));}
+        else {this.addDrawableChild(new TexturedButtonWidget(this.width/2-45, 12, 20, 20, 0, 0, 20, BOOKMARK_ENABLED_ICON, 32, 64, (button) -> {bookmarkedpage = -1; this.writeBookmark(); assert this.client != null; this.client.setScreen(this); }, new TranslatableText("jwg.button.bookmark")));}
 
         //Marker button to take you to the bookmarked page
         this.addDrawableChild(new TexturedButtonWidget(this.width/2-60, 9, 20, 20, 0, 0, 20, BOOKMARK_MARKER_ICON, 32, 64, (icon) -> {
             if (page != bookmarkedpage && bookmarkedpage >= 0) {
-                if (new File(pageLocation+"/"+bookmarkedpage+".jdat").exists()) {
-                    page = bookmarkedpage;
-                    assert this.client != null;
-                    this.client.setScreen(this);
-                } else {
-                    bookmarkedpage = -1;
-                }
-
+                if (new File(pageLocation+"/"+bookmarkedpage+".jdat").exists()) { page = bookmarkedpage; assert this.client != null; this.client.setScreen(this);
+                } else { bookmarkedpage = -1; }
             }
         }, new TranslatableText("jwg.button.bookmark-marker")));
 
@@ -126,14 +108,7 @@ public class menuScreen extends Screen {
         //Page buttons (arrows)
         int i = (this.width - 192) / 2;
         this.addDrawableChild(new PageTurnWidget(i + 116, 159, true, (button) -> {
-            if (page != pageLimit || pageLimit < 0 || !(page >= pageLimit) && pageLimit > 0) {
-                if (page >= pageLimit && pageLimit > 0) {
-                    page = pageLimit;
-                }
-                this.goToNextPage();
-                assert this.client != null;
-                this.client.setScreen(this);
-            }
+            if (page != pageLimit || pageLimit < 0) { if (page >= pageLimit && pageLimit > 0) { page = pageLimit; }this.goToNextPage(); assert this.client != null; this.client.setScreen(this); }
         }, this.pageTurnSound));
         this.addDrawableChild(new PageTurnWidget(i + 43, 159, false, (button) -> {
             this.goToPreviousPage();
@@ -141,11 +116,14 @@ public class menuScreen extends Screen {
             this.client.setScreen(this);
         }, this.pageTurnSound));
 
-        if (developerMode) {
-            this.versionText = "Coordinate Book "+version+" Developer build";
-        } else {
-            this.versionText = "Coordinate Book " + version;
-        }
+        if (developerMode) { this.versionText = "Coordinate Book "+version+" Developer build";
+        } else { this.versionText = "Coordinate Book " + version; }
+    }
+    //I have no clue what this does but i think it can do what i want it to
+    //It did not but if anyone wants to fix the rest of issue #22 be my guest
+    protected void selectionMgr() {
+        assert this.client != null;
+        this.selectionManager = new SelectionManager(() -> this.contents, (text) -> this.contents = text, SelectionManager.makeClipboardGetter(this.client), SelectionManager.makeClipboardSetter(this.client), (text) -> this.client.textRenderer.getWidth(text) <= this.loc);
     }
     protected void goToPreviousPage() {
         --page;
@@ -166,12 +144,13 @@ public class menuScreen extends Screen {
             }
         }
     }
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+    public void renderBookText(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, BOOK_TEXTURE);
         int i = (this.width - 192) / 2;
+
         this.drawTexture(matrices, i, 2, 0, 0, 192, 192);
         this.pageIndexText = new TranslatableText("book.pageIndicator", page + 1, Math.max((Objects.requireNonNull(new File(pageLocation+"/").list()).length), 1));
 
@@ -210,6 +189,57 @@ public class menuScreen extends Screen {
         }
         super.render(matrices, mouseX, mouseY, delta);
     }
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+       renderBookText(matrices, mouseX, mouseY, delta);
+    }
+    public boolean handleTextClick(Style style) {
+        assert style != null;
+        ClickEvent clickEvent = style.getClickEvent();
+        if (clickEvent == null) {
+            return false;
+        } else if (clickEvent.getAction() == ClickEvent.Action.CHANGE_PAGE) {
+            String string = clickEvent.getValue();
+
+            try {
+                int i = Integer.parseInt(string) - 1;
+                return this.goToPage(i);
+            } catch (Exception var5) {
+                return false;
+            }
+        } else {
+            boolean bl = super.handleTextClick(style);
+            if (bl && clickEvent.getAction() == ClickEvent.Action.RUN_COMMAND) {
+                this.closeScreen();
+            }
+
+            return bl;
+        }
+    }
+    private boolean goToPage(int i) {
+        page = i;
+        assert this.client != null;
+        this.client.setScreen(this);
+        return true;
+    }
+    public void closeScreen() {
+        assert this.client != null;
+        this.client.setScreen(null); }
+
+    public static int countPg() throws IOException {
+        File file = new File(pageLocation+"/"+page+".jdat");
+        FileInputStream fileInputStream;
+
+        { try { fileInputStream = new FileInputStream(file); } catch (FileNotFoundException e) { throw new RuntimeException(e); } }
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        int characterCount = 1;
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            characterCount += line.length();
+
+        }
+        return characterCount;
+    }
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
         if (button == 0) {
             Style style = this.getTextStyleAt(mouseX, mouseY);
@@ -220,77 +250,15 @@ public class menuScreen extends Screen {
 
         return super.mouseClicked(mouseX, mouseY, button);
     }
-    int o = 0;
-    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
-        int code = getExtendedKeyCodeForChar(keyCode);
-        char key = (char) code;
-        String keystring = String.valueOf(key).toLowerCase();
-        if (!deletePageButtonShown) {
-            if (code == 16777473) {
-                keystring = "";
-                removePage(page);
-            }
-        }
+    public boolean charTyped(char chr, int modifiers) {
 
-        if (developerMode) {
-            System.out.println(code + "\n" + key);
-        }
-
-        keystring = switch (code) {
-            case 192, 16777473, 16777559, 16777563, 16777481, 16777479, 16777547 -> "";
-            case 16777549 -> "-";
-            default -> keystring = keystring;
-        };
-        ++o;
-        if (code == 0) {
-            keystring = "";
-            nextCharacterSpecial = true;
-            o = 1;
-        } else if (code == 16777475 || code == 16777547){
-            keystring = "";
-            if (!Objects.equals(this.contents, "")) {
-                this.contents = this.contents.substring(0, this.contents.length() - 1);
-                try {
-                    FileWriter updatePage = new FileWriter(pageLocation+"/" + page + ".jdat");
-                    updatePage.write(this.contents);
-                    updatePage.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        if (nextCharacterSpecial && o == 2) {
-            keystring = keystring.toUpperCase(Locale.ROOT);
-            nextCharacterSpecial = false;
-            //Avert your eyes from this awful code
-            //If it works, don't touch it. And it does work, kinda...
-            //Needs improvement but it works-ish.
-            keystring = switch (keystring) {
-                case "1" -> "!";
-                case "2" -> "\"";
-                case "3" -> "£";
-                case "4" -> "$";
-                case "5" -> "%";
-                case "6" -> "^";
-                case "7" -> "&";
-                case "8" -> "*";
-                case "9" -> "(";
-                case "0" -> ")";
-                case "-" -> "_";
-                case "=" -> "+";
-                case "[" -> "{";
-                case "]" -> "}";
-                case ";" -> ":";
-                case "#" -> "~";
-                case "\\" -> "|";
-                case "," -> "<";
-                case "." -> ">";
-                case "/" -> "?";
-                case "'" -> "@";
-                default -> keystring;
-            };
-        }
+        try { this.loc = countPg(); } catch (IOException e) { throw new RuntimeException(e); }
+        this.ltchr = chr;
+        this.selectionManager.insert(chr);
+        String keystring = String.valueOf(this.ltchr);
         StringBuilder fulldata = new StringBuilder();
+        this.selectionManager.putCursorAtEnd();
+        this.selectionManager.insert(chr);
         try {
             Scanner readPageContent = new Scanner(new File(pageLocation+"/"+page+".jdat"));
             while (readPageContent.hasNextLine()) {
@@ -314,24 +282,82 @@ public class menuScreen extends Screen {
             e.printStackTrace();
         }
         Objects.requireNonNull(this.client).setScreen(this);
-        return false;
+        return true;
+    }
+
+
+    public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
+        if (keyCode == 259) {
+            StringBuilder fulldata = new StringBuilder();
+            try {
+                Scanner readPageContent = new Scanner(new File(pageLocation+"/"+page+".jdat"));
+                while (readPageContent.hasNextLine()) {
+                    String data = readPageContent.nextLine();
+                    if (!fulldata.toString().equals("")) {
+                        data = "\n" + data;
+                    }
+                    fulldata.append(data);
+                }
+                readPageContent.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (!fulldata.toString().equals("")) {
+                fulldata = new StringBuilder(fulldata.substring(0, this.contents.length() - 1));
+                this.contents = String.valueOf(fulldata);
+                try {
+                    FileWriter updatePage = new FileWriter(pageLocation+"/"+page+".jdat");
+                    updatePage.write(String.valueOf(fulldata));
+                    updatePage.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        if (keyCode == 257) {
+            StringBuilder fulldata = new StringBuilder();
+            try {
+                Scanner readPageContent = new Scanner(new File(pageLocation+"/"+page+".jdat"));
+                while (readPageContent.hasNextLine()) {
+                    String data = readPageContent.nextLine();
+                    if (!fulldata.toString().equals("")) {
+                        data = "\n" + data;
+                    }
+                    fulldata.append(data);
+                }
+                readPageContent.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (!fulldata.toString().equals("")) {
+                fulldata = new StringBuilder(fulldata + "\n ");
+                this.contents = String.valueOf(fulldata);
+                try {
+                    FileWriter updatePage = new FileWriter(pageLocation+"/"+page+".jdat");
+                    updatePage.write(String.valueOf(fulldata));
+                    updatePage.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return true;
     }
     @Nullable
     public Style getTextStyleAt(double x, double y) {
-        if (this.cachedPage.isEmpty()) {
-            return null;
-        } else {
-            int i = MathHelper.floor(x - (double)((this.width - 192) / 2) - 36.0);
+        if (!this.cachedPage.isEmpty()) {
+            int i = MathHelper.floor(x - (double) ((this.width - 192) / 2) - 36.0);
             int j = MathHelper.floor(y - 2.0 - 30.0);
             if (i >= 0 && j >= 0) {
                 Objects.requireNonNull(this.textRenderer);
                 int k = Math.min(128 / 9, this.cachedPage.size());
                 if (i <= 114) {
-                    Objects.requireNonNull(Objects.requireNonNull(this.client).textRenderer);
+                    assert this.client != null;
+                    Objects.requireNonNull(this.client.textRenderer);
                     if (j < 9 * k + k) {
                         Objects.requireNonNull(this.client.textRenderer);
                         int l = j / 9;
-                        if (l >= 0 && l < this.cachedPage.size()) {
+                        if (l < this.cachedPage.size()) {
                             OrderedText orderedText = this.cachedPage.get(l);
                             return this.client.textRenderer.getTextHandler().getStyleAt(orderedText, i);
                         }
@@ -340,10 +366,9 @@ public class menuScreen extends Screen {
                     }
                 }
 
-                return null;
-            } else {
-                return null;
             }
         }
+        return null;
     }
+
 }
