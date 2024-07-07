65f67e08c1960227c305db9e55e47e32f7ab970b
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:22:15 +0100

    New texture for bookmark button

--- src/main/java/com/jwg/notebook/gui/sidebar.java
+++ src/main/java/com/jwg/notebook/gui/sidebar.java
@@ -15,8 +15,8 @@ import static com.jwg.notebook.screens.menuScreen.bookmarkedpage;
 import static com.jwg.notebook.screens.menuScreen.page;
 
 public class sidebar {
-    public static TexturedButtonWidget addSidebarButton(int pos, Identifier BUTTON_TEXTURE, Screen BOOK_MENU, String buttonID) {
-        return new TexturedButtonWidget(BOOK_MENU.width/2 +90, 18+(12*pos), 8, 8, 0, 0, 8, BUTTON_TEXTURE, 8, 16, button -> buttonAction(buttonID), Text.translatable("jwg.button."+buttonID));
+    public static TexturedButtonWidget addSidebarButton(int pos, Identifier BUTTON_TEXTURE, Screen BOOK_MENU, String buttonID, int size) {
+        return new TexturedButtonWidget(BOOK_MENU.width/2 +90, 18+(12*pos), 8, 8, 0, 0, size, BUTTON_TEXTURE, size, size*2, button -> buttonAction(buttonID), Text.translatable("jwg.button."+buttonID));
     }
     static void buttonAction(String buttonID) {
         if (Objects.equals(buttonID, "delete")) {
