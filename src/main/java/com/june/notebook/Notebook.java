a0d96f67da302e4d4a251a82bc8d3f42320fb3b4
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:42 +0100

    Implemented lolcat and Portuguese translations (From <@627553610070360084>), fixed bug where preset text would be off center for longer sentences

--- src/main/java/com/june/notebook/Notebook.java
+++ src/main/java/com/june/notebook/Notebook.java
@@ -18,7 +18,7 @@ public class Notebook implements ModInitializer {
 	public static boolean presetsEnabled = true;
 	public static String verified_page_location = "Notebook/Default";
 	public static final boolean developerMode = false;
-	public static final String version = "2.1.2";
+	public static final String version = "2.1.3";
 	public static final String project = "Vanilla-Notebook";
 	public static String pageLocation = "Notebook/Default";
 	public static final Logger LOGGER = LoggerFactory.getLogger(project);
