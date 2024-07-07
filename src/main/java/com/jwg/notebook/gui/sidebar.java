b9539b7cc42eb6602f12cb6dc532d04a2c178537
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:26 +0100

    Fixed page deleting and updated version count

--- src/main/java/com/jwg/notebook/gui/sidebar.java
+++ src/main/java/com/jwg/notebook/gui/sidebar.java
@@ -7,6 +7,7 @@ import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import java.awt.*;
 import java.io.File;
 import java.util.Objects;
 
@@ -15,18 +16,7 @@ import static com.jwg.notebook.screens.menuScreen.bookmarkedpage;
 import static com.jwg.notebook.screens.menuScreen.page;
 
 public class sidebar {
-    public static TexturedButtonWidget addSidebarButton(int pos, Identifier BUTTON_TEXTURE, Screen BOOK_MENU, String buttonID, int size) {
-        return new TexturedButtonWidget(BOOK_MENU.width/2 +90, 18+(12*pos), 8, 8, 0, 0, size, BUTTON_TEXTURE, size, size*2, button -> buttonAction(buttonID), Text.translatable("jwg.button."+buttonID));
+    public static TexturedButtonWidget addSidebarButton(int pos, Identifier BUTTON_TEXTURE, Screen BOOK_MENU, String buttonID, int size, ButtonWidget.PressAction pressAction) {
+        return new TexturedButtonWidget(BOOK_MENU.width/2 +90, 18+(12*pos), 8, 8, 0, 0, size, BUTTON_TEXTURE, size, size*2, pressAction, Text.translatable("jwg.button."+buttonID));
     }
-    static void buttonAction(String buttonID) {
-        if (Objects.equals(buttonID, "delete")) {
-            menuScreen.removePage(page);
-        } else if (Objects.equals(buttonID, "bookmark")) {
-            if (page != bookmarkedpage && bookmarkedpage >= 0) {
-                if (new File(pageLocation+"/"+bookmarkedpage+".jdat").exists()) { page = bookmarkedpage;
-                } else { bookmarkedpage = -1; }
-            }
-        }
-    }
-
 }
