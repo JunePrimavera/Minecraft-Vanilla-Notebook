edc88cc3f2634af91e634e87ee08e87c04218e33
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:22:14 +0100

    Make latest

---
+++ src/main/java/com/jwg/coord_book/util/ensureFileStructureExists.java
@@ -0,0 +1,22 @@
+package com.jwg.coord_book.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ensureFileStructureExists {
+    public static boolean exists(String folders) {
+        return Files.exists(Path.of(folders));
+    }
+
+    public static void createFiles(boolean exists) {
+        if (!exists) {
+            try {
+                Files.createDirectories(Paths.get("CoordinateBook/"));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
