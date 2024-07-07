43aed1992f1c5807bf08f0faeab09d21ccd6216c
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:25 +0100

    Fixed issue with infinite limit, and added comments to cfg file

--- src/main/java/com/jwg/coord_book/util/resetConfig.java
+++ src/main/java/com/jwg/coord_book/util/resetConfig.java
@@ -7,7 +7,7 @@ public class resetConfig {
     public void reset() {
         try {
             FileWriter fileOverwriter = new FileWriter("config/coordinate-book/config.cfg");
-            fileOverwriter.write("pagedirectory=CoordinateBook\npagelimit=-1");
+            fileOverwriter.write("# You need to reload for these to take affect!\n# Directory for the pages to be stored in (e.g .minecraft/CoordinateBook)\npagedirectory=CoordinateBook\n# Set page limits, negative numbers mean no limit\npagelimit=-1");
             fileOverwriter.close();
         } catch (IOException e) {
             e.printStackTrace();
