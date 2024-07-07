6c7feb5a10a187401c91d43981be5cb12a0b02b6
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:22:15 +0100

    Implemented configurable button offsets (Fixing issue #50 )

--- src/main/java/com/june/notebook/Notebook.java
+++ src/main/java/com/june/notebook/Notebook.java
@@ -6,15 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static com.june.notebook.Keybind.openBookKeybindRegister;
 
 public class Notebook implements ModInitializer {
+	public static int[] button_positions = { 0, 0, 0, 0 };
 	public static boolean presetsEnabled = true;
 	public static String verified_page_location = "Notebook/Default";
 	public static final boolean developerMode = false;
-	public static final String version = "2.1.1";
+	public static final String version = "2.1.2";
 	public static final String project = "Vanilla-Notebook";
 	public static String pageLocation = "Notebook/Default";
 	public static final Logger LOGGER = LoggerFactory.getLogger(project);
@@ -60,6 +64,58 @@ public class Notebook implements ModInitializer {
 				throw new RuntimeException(e);
 			}
 		}
+		// Make config for button placement if they don't already exist
+		if (!new File("config/vanilla-notebook/buttons/mainmenu.jdat").exists()) {
+			boolean b = new File("config/vanilla-notebook/buttons/").mkdirs();
+			try {
+				b = new File("config/vanilla-notebook/buttons/mainmenu.jdat").createNewFile();
+				FileWriter w = new FileWriter("config/vanilla-notebook/buttons/mainmenu.jdat");
+				w.write("0\n0");
+				w.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		if (!new File("config/vanilla-notebook/buttons/gamemenu.jdat").exists()) {
+			boolean b = new File("config/vanilla-notebook/buttons/").mkdirs();
+			try {
+				b = new File("config/vanilla-notebook/buttons/gamemenu.jdat").createNewFile();
+				FileWriter w = new FileWriter("config/vanilla-notebook/buttons/gamemenu.jdat");
+				w.write("0\n0");
+				w.close();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		// Read config for button placement
+		try {
+			File f = new File("config/vanilla-notebook/buttons/mainmenu.jdat");
+			Scanner myReader = new Scanner(f);
+			int i = 0;
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				button_positions[i+2] = Integer.parseInt(data);
+				i++;
+				if (i > 1) {
+					break;
+				}
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) { e.printStackTrace(); }
+		try {
+			File f = new File("config/vanilla-notebook/buttons/gamemenu.jdat");
+			Scanner myReader = new Scanner(f);
+			int i = 0;
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				button_positions[i] = Integer.parseInt(data);
+				i++;
+				if (i > 1) {
+					break;
+				}
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) { e.printStackTrace(); }
 		// Register keybind
 		if (is_p_init) {
 			LOGGER.info("Registering keybinds...");
