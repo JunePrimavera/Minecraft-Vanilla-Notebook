eed195516c5f4d6e7c6c3b97025adcac9d8935cf
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:22:15 +0100

    Fixed crash on first time use

--- src/main/java/com/jwg/notebook/util/resetConfig.java
+++ src/main/java/com/jwg/notebook/util/resetConfig.java
@@ -7,7 +7,7 @@ public class resetConfig {
     public static void reset() {
         try {
             FileWriter fileOverwriter = new FileWriter("config/vanilla-notebook/config.cfg");
-            fileOverwriter.write("# You need to reload for these to take affect!\n# Directory for the pages to be stored in (e.g .minecraft/CoordinateBook)\npagedirectory=Notebook\n# Set page limits, negative numbers mean no limit\npagelimit=-1\n# Page to start on after opening the book\nstartpage=0");
+            fileOverwriter.write("# You need to reload for these to take affect!\n# Directory for the pages to be stored in (e.g .minecraft/CoordinateBook)\npagedirectory=Notebook/Default\n# Set page limits, negative numbers mean no limit\npagelimit=-1\n# Page to start on after opening the book\nstartpage=0");
             fileOverwriter.close();
         } catch (IOException e) { e.printStackTrace(); }
 
