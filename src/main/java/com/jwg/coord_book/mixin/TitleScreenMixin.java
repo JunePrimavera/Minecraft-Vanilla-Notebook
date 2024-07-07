bb9d6dff69bf0c655b31af6286adf35b9b69f9c5
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:41 +0100

    Initial Commit

---
+++ src/main/java/com/jwg/coord_book/mixin/TitleScreenMixin.java
@@ -0,0 +1,15 @@
+package com.jwg.coord_book.mixin;
+
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+	@Inject(method = "init", at = @At("TAIL"))
+	public void onInit(CallbackInfo ci) {
+
+	}
+}
