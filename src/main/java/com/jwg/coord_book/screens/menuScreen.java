30890b7e1212e7cbffc9030dcce2ea39f611eef8
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:14:44 +0100

    Most of the gui is complete

--- src/main/java/com/jwg/coord_book/screens/menuScreen.java
+++ src/main/java/com/jwg/coord_book/screens/menuScreen.java
@@ -19,6 +19,11 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.*;
 import net.minecraft.util.Identifier;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static com.jwg.coord_book.CoordBook.*;
 import static net.minecraft.client.gui.screen.ingame.BookScreen.EMPTY_PROVIDER;
 
@@ -26,16 +31,15 @@ import static net.minecraft.client.gui.screen.ingame.BookScreen.EMPTY_PROVIDER;
 public class menuScreen extends Screen {
     static int page = 0;
     public static Text PAGENO = Text.literal("Page " + page);
+    public static String fileToRead = "CoordinateBook/"+page+".json";
     public static final Identifier BOOK_TEXTURE = new Identifier("textures/gui/book.png");
 
     public menuScreen(BookScreen.Contents contents) {
         this(contents, true);
     }
-
     public menuScreen() {
         this(EMPTY_PROVIDER, false);
     }
-
     public menuScreen(BookScreen.Contents contents, boolean bl) {
         super(NarratorManager.EMPTY);
     }
@@ -51,6 +55,12 @@ public class menuScreen extends Screen {
         }));
     }
     protected void addMenuButtons() {
+        try {
+            this.addDrawableChild(new PlainTextButtonWidget(this.width - 192 / 2, 15, textRenderer.getWidth(Files.readString(Paths.get(fileToRead))), 10, Text.literal(Files.readString(Paths.get(fileToRead))), (buttonWidget) -> {
+            }, this.textRenderer));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         this.addDrawableChild(new PlainTextButtonWidget(this.width - textRenderer.getWidth(PAGENO) - 2, this.height - 10, textRenderer.getWidth(PAGENO), 10, PAGENO, (buttonWidget) -> {
         }, this.textRenderer));
 
@@ -62,12 +72,29 @@ public class menuScreen extends Screen {
             PAGENO = Text.literal("Page " + page);
             assert this.client != null;
             this.client.setScreen(this);
+            fileToRead = "CoordinateBook/"+page+".json";
         }, Text.translatable("jwg.button.back")));
         this.addDrawableChild(new TexturedButtonWidget(this.width / 2 + 100, 196, 20, 20, 0, 0, 20, NEXT_ICON, 32, 64, (button) -> {
             page = page+1;
             PAGENO = Text.literal("Page " + page);
             assert this.client != null;
             this.client.setScreen(this);
+            try {
+                File pageF = new File("CoordinateBook/"+page+".json");
+                if (pageF.createNewFile()){
+                    LOGGER.info("Created page " + page);
+                }
+                else{
+                    LOGGER.info("Page {} already exists, reading instead..", page);
+                    //TODO: Read & display page
+                    fileToRead = "CoordinateBook/"+page+".json";
+                }
+
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }, Text.translatable("jwg.button.forwards")));
 
     }
