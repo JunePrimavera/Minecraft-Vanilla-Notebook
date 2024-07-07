7eec645e327fc6f57172896a2b30c6174dc16493
Juniper Primavera <juniper.gardiner@gmail.com>
2024-07-07 14:24:42 +0100

    Updated version to 2.1.0, refactored and made presets enabled by default

--- src/main/java/com/june/notebook/gui/button/delete.java
+++
@@ -1,9 +0,0 @@
-package com.june.notebook.gui.button;
-
-import static com.june.notebook.util.removePage.remove;
-
-public class delete {
-    public static void onPress(int page) { remove(page); }
-
-
-}
